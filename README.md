98aca756edad7c634c6de4667a633e3fd36b98b7
John Nieri <niericentral@gmail.com>
2013-04-02 15:47:32 +0900

    - gitignore setup - initial readme - initial setup.py

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 PQHelper
 ========
+
+Vision
+======
+
+PQHelper takes any on-screen game of Puzzle Quest(tm), simulates one or more
+ turns, and then gives the user a ranked list of all possible actions.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PQHelper',
+    version='0.0.0',
+    packages=[''],
+    url='http://github.com/kobejohn/PQHelper',
+    license='MIT',
+    author='KobeJohn',
+    author_email='niericentral@gmail.com',
+    description='Simulate, analyze, and advise on the game called Puzzle Quest '
+)
