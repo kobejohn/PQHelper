e0f1ba82187658ae991a329bb9fbafbce0f2bbfe
John Nieri <niericentral@gmail.com>
2013-04-30 16:30:42 +0900

    Specified and implemented versus scoring algorithm. Includes specification and implementation of custom versus Actor.

--- pqhelper/versus.py
+++ pqhelper/versus.py
@@ -1,9 +1,155 @@
 from pqhelper import base as _base
 
 
+class _Actor(_base.Actor):
+    def __init__(self, name=None, health=100, r=0, g=0, b=0, y=0):
+        super(_Actor, self).__init__(name=name)
+        self.health = health
+        self.r = r
+        self.g = g
+        self.b = b
+        self.y = y
+        self._attributes_to_copy.extend(('health', 'r', 'g', 'b', 'y'))
+
+    def score_eot(self, eot):
+        """Return a score for this end of turn based on how favorable it is
+        for this actor. Higher numbers are better for self."""
+        state = eot.parent
+        final_self = {'player': state.player,
+                      'opponent': state.opponent}[self.name]
+        return sum((final_self.health,
+                    final_self.r, final_self.g, final_self.b, final_self.y))
+
+    def consume_tile_groups(self, tile_groups):
+        """Apply the tiles to self and return an attack value."""
+        attack_value = 0
+        for tile_group in tile_groups:
+            group_count = 0
+            group_type = None
+            wildcard_multiplier = 1  # start with no wildcard multiplier
+            for tile in tile_group:
+                # handle wildcards
+                if tile.is_wildcard():
+                    wildcard_multiplier += int(tile._type)
+                    continue
+                # handle skullbombs
+                elif tile.is_skullbomb():
+                    group_count += 5
+                    group_type = _State.Tile('s')
+                else:
+                    group_count += 1
+                    group_type = tile
+            group_count *= wildcard_multiplier
+            attack_value += self._consume_tiles(group_type, group_count)
+        return attack_value
+
+    def _consume_tiles(self, base_tile, count):
+        """Accept any base tile (color, money, experience or skull) apply
+        the given count of them to self and return an attack value."""
+        base_type = base_tile._type
+        # colors, money, experience
+        if any((base_tile.is_color(),
+                base_tile.is_money(),
+                base_tile.is_experience())):
+            original_value = getattr(self, base_type)
+            setattr(self, base_type, original_value + count)
+            attack_value = 0
+        # skulls
+        elif any((base_tile.is_skullbomb(),
+                  base_tile.is_skull())):
+            attack_value = count
+        else:
+            raise ValueError('Unexpected base tile type: {}'.format(base_type))
+        return attack_value
+
+    def apply_attack(self, attack_value):
+        """Apply the attack value to self.
+
+        Stub for specific game types
+        """
+        self.health -= attack_value
+
+
 class _State(_base.State):
     _use_random_fill = True
-    pass  # eventually actor will be customized and placed here
+    Actor = _Actor
+
+
+def realistic_choices(root):
+    """Return a list of choices and the simulated final results of each choice,
+    assuming that the player and opponent both make realistic choices
+    along the way.
+    """
+    # store per-turn results from the bottom up. root results are under None
+    realistic_results_by_EOT = {None: list()}
+    for node in root._node.post_order_nodes:
+        # only looking for EOT states (EOT or manadrain)
+        if not isinstance(node.main, _base.EOT):
+            continue
+        eot = node.main
+        # by the time this eot is reached, all its children have been handled
+        # so get all the realistic results below it
+        try:  # get the results stored previously in the higher turn
+            realistic_results = realistic_results_by_EOT[eot]
+        except KeyError:  # leaves will be empty and are own realistic result
+            realistic_results = [_summarize_eot(eot)]
+        # get the most realistic result for this eot
+        is_players_turn = eot.parent.active is eot.parent.player
+        realistic_result = sorted(realistic_results, reverse=is_players_turn)[0]
+        # place the most realistic result on the parent EOT's list
+        parent = eot.parent
+        while parent:
+            if isinstance(parent, _base.EOT):
+                break  # stop when the parent EOT is found
+            parent = parent.parent
+        # at this point the parent is either None (root) or another EOT
+        # so put the most realistic result of this EOT on the parent's list
+        realistic_results_by_EOT.setdefault(parent,
+                                            list()).append(realistic_result)
+    root_results = realistic_results_by_EOT[None]  # get the root's results
+    return sorted(root_results, reverse=True)
+
+
+def _summarize_eot(eot):
+    """Summarize the value of this EOT and the first step to get to it.
+
+    Arguments:
+    eot - an EOT / ManaDrain transition in a simulation tree.
+
+    Return:
+    a dict with the following items:
+    - overall_score (higher numbers favor player, lower favor opponent)
+    - root_action (the details of the root action that led to this state)
+    """
+    overall_score = _score_eot(eot)
+    action_summary = _summarize_root_action(eot)
+    return {'overall_score': overall_score,
+            'root_action': action_summary}
+
+
+def _score_eot(eot):
+    """Return an overall score for this eot."""
+    player = eot.parent.player
+    opponent = eot.parent.opponent
+    return player.score_eot(eot) - opponent.score_eot(eot)
+
+
+def _summarize_root_action(eot):
+    """Return a summary of the root action that led to this EOT.
+
+    Action Summary Keys:
+    - type
+    - details
+    """
+    # find the action leading back to the root
+    node = eot.parent
+    child = eot
+    while node.parent:  # search for the root
+        child = node
+        node = node.parent
+    action = child  # at this point, node is root, child is the action
+    return {'type': action.type,
+            'details': action.position_pair}
 
 
 def _create_simulation_root(board_string,
@@ -22,12 +168,6 @@ def _expand_simulation(root, turns=1):
     # list makes the generator complete the simulation to the given turn
     list(root.end_of_turns(absolute_turn_depth=turns))
     return root
-
-# def scored_actions(board_string, player_string, opponent_string):
-#     pass
-#
-# def _continue_simulation(state, turns=2):
-#     pass
 
 
 if __name__ == '__main__':
--- tests/unit/test_versus.py
+++ tests/unit/test_versus.py
@@ -1,10 +1,72 @@
 import unittest
 from mock import patch
 
+from pqhelper.versus import realistic_choices, _summarize_eot
+from pqhelper.versus import _score_eot, _summarize_root_action
 from pqhelper.versus import _create_simulation_root, _expand_simulation
 
 
-class Test_simulation(unittest.TestCase):
+class Test_Scoring(unittest.TestCase):
+    @patch('pqhelper.versus._State._random_fill', False)
+    def setUp(self):
+        # run a short simulation for testing; 6 leaves
+        self.board_string_3_valid_swaps = '........\n' \
+                                          '........\n' \
+                                          '........\n' \
+                                          '........\n' \
+                                          '........\n' \
+                                          'r..g..b.\n' \
+                                          'r..g..b.\n' \
+                                          'xr.xg.xb'
+        self.root = _create_simulation_root(self.board_string_3_valid_swaps)
+
+    def test_realistic_choices_returns_an_empty_sequence_for_root_only(self):
+        choices = realistic_choices(self.root)
+        self.assertSequenceEqual(choices, tuple())
+
+    def test_realistic_choices_has_3_results(self):
+        _expand_simulation(self.root, 1)
+        choices = realistic_choices(self.root)
+        self.assertEqual(len(choices), 3)
+
+    def test__summarize_eot_provides_required_items(self):
+        # Combined specification for simplicity
+        _expand_simulation(self.root, 1)
+        any_leaf = self.root._node.leaves.next().main
+        summary = _summarize_eot(any_leaf)
+        # confirm overall score
+        keys_spec = ('overall_score', 'root_action')
+        self.assertItemsEqual(summary.keys(), keys_spec)
+
+    def test__score_eot_with_default_actors_scores_positive_for_1_turn(self):
+        one_turn = 1
+        _expand_simulation(self.root, one_turn)
+        any_leaf = self.root._node.leaves.next().main
+        score = _score_eot(any_leaf)
+        self.assertGreater(score, 0)
+
+    def test__score_eot_with_default_actors_scores_zero_for_2_turns(self):
+        # default actors have same preferences so 3 of any color
+        two_turns = 2
+        _expand_simulation(self.root, two_turns)
+        any_leaf = self.root._node.leaves.next().main
+        score = _score_eot(any_leaf)
+        self.assertEqual(score, 0)
+
+    def test__summarize_root_action_returns_one_of_valid_swap_pairs(self):
+        _expand_simulation(self.root, 1)
+        any_leaf = self.root._node.leaves.next().main
+        root_action_summary = _summarize_root_action(any_leaf)
+        # confirm the type of the root action
+        self.assertEqual(root_action_summary['type'], 'swap')
+        # confirm the swap is one of the valid swaps
+        valid_swap_pairs_spec = (((7, 0), (7, 1)),
+                                 ((7, 3), (7, 4)),
+                                 ((7, 6), (7, 7)))
+        self.assertIn(root_action_summary['details'], valid_swap_pairs_spec)
+
+
+class Test_Simulation(unittest.TestCase):
     # Test attributes
     board_string_3_valid_swaps = '........\n' \
                                  '........\n' \
