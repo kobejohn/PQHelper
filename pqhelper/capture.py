fdf998ef3b1e424d4ba57f7d6de0578ae5b1f670
John Nieri <niericentral@gmail.com>
2013-05-07 16:27:19 +0900

    Partly respecified and partly reimplemented capture. Mostly to match with new Game/State structure. Importantly though fixed a huge bad-logic bug in the duplicate board detector. It was stopping the detection process when the first different tile was found. Then the same board can be detected many times until the whole board is added to the duplicate tree.

--- pqhelper/capture.py
+++ pqhelper/capture.py
@@ -1,36 +1,40 @@
-from pqhelper import base as _base
+from pqhelper import base
 
 
-class _DuplicateTree(_base.TreeNode):
+class _DuplicateTree(base.TreeNode):
     def __init__(self, tile=None):
         super(_DuplicateTree, self).__init__(tile=tile)
 
     def find_or_graft(self, board):
+        """Build a tree with various tiles for each position...."""
         node = self
-        for row in range(8):
-            for col in range(8):
-                tile = board[row, col]
-                found_tile = False
-                for child in node.children:
-                    if child.tile == tile:
-                        node = child
-                        found_tile = True
-                        break  # found an existing match
-                if not found_tile:
-                    # no existing matches found so attach it
-                    # it already can't be a duplicate so stop here
-                    child = _DuplicateTree(tile)
-                    node.graft_child(child)
-                    return False
-        return True  # finally return True if all tiles were grafted previously
+        exact_match = True  # assume same until find a difference
+        # compare each position
+        for p, new_tile in board.positions_with_tile():
+            found_tile = False  # assume no tile in same position until found
+            for child in node.children:
+                if child.tile == new_tile:
+                    # same tile found in this position --> continue this branch
+                    node = child
+                    found_tile = True
+                    break
+            if found_tile:
+                pass  # go on to the next position
+            else:
+                # different tile --> start new branch and mark not exact match
+                child = _DuplicateTree(new_tile)
+                node.graft_child(child)
+                node = child
+                exact_match = False  # this will get set many times. no problem
+        return exact_match
 
 
-class _State(_base.State):
-    __duplicate_root = _DuplicateTree()
+class Game(base.Game):
+    _duplicate_root = _DuplicateTree()
 
     @classmethod
     def clear_duplicate_tree(cls):
-        for child in cls.__duplicate_root.children:
+        for child in cls._duplicate_root.children:
             child.trim()
 
     def _disallow_state(self, state):
@@ -40,16 +44,16 @@ class _State(_base.State):
 
     def __duplicate_board(self, state):
         """Disallow any board that has been simulated elsewhere."""
-        return self.__duplicate_root.find_or_graft(state.board)
+        return self._duplicate_root.find_or_graft(state.board)
 
     def __impossible_by_count(self, state):
         """Disallow any board that has insufficient tile count to solve."""
         # count all the tile types and name them for readability
-        counts = {tile_type: 0 for tile_type in self.Tile._all_types}
+        counts = {tile_type: 0 for tile_type in base.Tile._all_types}
         standard_wildcard_type = '2'
-        for p in state.board.positions():
+        for p, tile in state.board.positions_with_tile():
             # count all wildcards as one value
-            tile_type = state.board[p]._type
+            tile_type = tile._type
             try:
                 int(tile_type)
                 counts[standard_wildcard_type] += 1
@@ -80,28 +84,25 @@ class _State(_base.State):
         return False
 
 
-_Board = _State.Board  # convenience access to Board
-
-
-def capture(board_string):
-    """Try to solve the board described by board_string.
-
-    Return tuple of swaps in the order required to solve the board.
-    """
-    _State.clear_duplicate_tree()
-    state = _State(_Board(board_string))
-    solution_sequence = list()
-    enough_turns = 20
-    for eot in state.end_of_turns(absolute_turn_depth=enough_turns):
-        if eot.parent.board.is_empty():
-            node = eot._node
-            while node:
-                if node.main.type == 'swap':
-                    solution_sequence.append(node.main.position_pair)
-                node = node.parent
-            break
-    _State.clear_duplicate_tree()
-    return tuple(reversed(solution_sequence))
+# def capture(board_string):
+#     """Try to solve the board described by board_string.
+#
+#     Return tuple of swaps in the order required to solve the board.
+#     """
+#     _State.clear_duplicate_tree()
+#     state = _State(_Board(board_string))
+#     solution_sequence = list()
+#     enough_turns = 20
+#     for eot in state.end_of_turns(absolute_turn_depth=enough_turns):
+#         if eot.parent.board.is_empty():
+#             node = eot._node
+#             while node:
+#                 if node.main.type == 'swap':
+#                     solution_sequence.append(node.main.position_pair)
+#                 node = node.parent
+#             break
+#     _State.clear_duplicate_tree()
+#     return tuple(reversed(solution_sequence))
 
 
 if __name__ == '__main__':
--- tests/integration/test_capture.py
+++ tests/integration/test_capture.py
@@ -1,108 +1,144 @@
-# import unittest
-#
-# from pqhelper.capture import _State as State, _Board as Board, capture
-#
-#
-# class Test_capture(unittest.TestCase):
-#     def test_capture_on_easy_board_returns_correct_solution(self):
-#         board_string = '........\n' \
-#                        '........\n' \
-#                        '........\n' \
-#                        '........\n' \
-#                        '.......x\n' \
-#                        '....xx.r\n' \
-#                        '....rr.r\n' \
-#                        '..rryyry'
-#         solution = capture(board_string)
-#         # confirm that the solution matches the specification
-#         solution_spec = (((7, 6), (7, 7)), ((7, 6), (7, 7)))
-#         self.assertSequenceEqual(solution, solution_spec)
-#
-#
-# class Test_Capture_State(unittest.TestCase):
-#     def setUp(self):
-#         State.clear_duplicate_tree()
-#
-#     def test__disallow_state_disallows_duplicate_boards(self):
-#         board_string = '........\n' \
-#                        '........\n' \
-#                        '........\n' \
-#                        '........\n' \
-#                        '........\n' \
-#                        '........\n' \
-#                        '........\n' \
-#                        'ss......'
-#         state_1 = State(Board(board_string))
-#         state_2 = State(Board(board_string))
-#         self.assertTrue(state_1._disallow_state(state_2))
-#
-#     def test__disallow_state_allows_skullbomb_with_enough_skulls(self):
-#         board_string = '........\n' \
-#                        '........\n' \
-#                        '........\n' \
-#                        '.......m\n' \
-#                        '........\n' \
-#                        '.......m\n' \
-#                        '.......m\n' \
-#                        'ss..*..m'
-#         state = State(Board(board_string))
-#         self.assertFalse(state._disallow_state(state))
-#
-#     def test__disallow_state_allows_wildcard_with_enough_of_any_color(self):
-#         board_string = '........\n' \
-#                        '........\n' \
-#                        '........\n' \
-#                        '.......m\n' \
-#                        '........\n' \
-#                        '.......m\n' \
-#                        '.......m\n' \
-#                        'rr..4..m'
-#         state = State(Board(board_string))
-#         self.assertFalse(state._disallow_state(state))
-#
-#     def test__disallow_state_disallows_too_few_x_m_s_or_color(self):
-#         # combined specification for simplicity
-#         board_strings = ('........\n'
-#                          '........\n'
-#                          '........\n'
-#                          '.......m\n'
-#                          '........\n'
-#                          '.......m\n'
-#                          '.......m\n'
-#                          'xx.....m',
-#
-#                          '........\n'
-#                          '........\n'
-#                          '........\n'
-#                          '.......x\n'
-#                          '........\n'
-#                          '.......x\n'
-#                          '.......x\n'
-#                          'mm.....x',
-#
-#                          '........\n'
-#                          '........\n'
-#                          '........\n'
-#                          '.......m\n'
-#                          '........\n'
-#                          '.......m\n'
-#                          '.......m\n'
-#                          'ss.....m',
-#
-#                          '........\n'
-#                          '........\n'
-#                          '........\n'
-#                          '.......m\n'
-#                          '........\n'
-#                          '.......m\n'
-#                          '.......m\n'
-#                          'rr.....m')
-#         for board_string in board_strings:
-#             state = State(Board(board_string))
-#             self.assertTrue(state._disallow_state(state),
-#                             'This board unexpectedly passed impossible count:'
-#                             '\n{}'.format(board_string))
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
+import unittest
+
+from pqhelper.base import Board, State, Actor
+from pqhelper.capture import Game
+
+
+class Test_capture(unittest.TestCase):
+    def test_capture_on_easy_board_returns_correct_solution(self):
+        board_string = '........\n' \
+                       '........\n' \
+                       '........\n' \
+                       '........\n' \
+                       '.......x\n' \
+                       '....xx.r\n' \
+                       '....rr.r\n' \
+                       '..rryyry'
+        solution = capture(board_string)
+        # confirm that the solution matches the specification
+        solution_spec = (((7, 6), (7, 7)), ((7, 6), (7, 7)))
+        self.assertSequenceEqual(solution, solution_spec)
+
+
+class Test_Capture_Game(unittest.TestCase):
+    def test__disallow_state_disallows_duplicate_boards(self):
+        board_string = '........\n' \
+                       '........\n' \
+                       '........\n' \
+                       '........\n' \
+                       '....m...\n' \
+                       '....m...\n' \
+                       '....s...\n' \
+                       'ss..m..s'
+        result_board_string = '........\n' \
+                              '........\n' \
+                              '........\n' \
+                              '........\n' \
+                              '........\n' \
+                              '........\n' \
+                              '........\n' \
+                              'ss..s..s'
+        game = generic_game()
+        root = generic_state(board=Board(board_string))
+        # simulate the game
+        list(game.ends_of_turn(root=root))
+        # confirm that the game will now disallow a state with the same result
+        test_state = generic_state(board=Board(result_board_string))
+        self.assertTrue(game._disallow_state(test_state),
+                        'Duplicate board unexpectedly passed duplicate'
+                        ' detector. This board:\n{}\nshould have been detected'
+                        'as a duplicate within the simulation of this start'
+                        'board:\n{}'.format(result_board_string, board_string))
+
+    def test__disallow_state_allows_skullbomb_with_enough_skulls(self):
+        board_string = '........\n' \
+                       '........\n' \
+                       '........\n' \
+                       '.......m\n' \
+                       '........\n' \
+                       '.......m\n' \
+                       '.......m\n' \
+                       'ss..*..m'
+        game = generic_game()
+        state = generic_state(Board(board_string))
+        self.assertFalse(game._disallow_state(state),
+                         'Unexpectedly failed state that has at least one'
+                         ' skullbomb and enough total skulls to explode it:\n{}'
+                         ''.format(state.board))
+
+    def test__disallow_state_allows_wildcard_with_enough_of_any_color(self):
+        board_string = '........\n' \
+                       '........\n' \
+                       '........\n' \
+                       '.......m\n' \
+                       '........\n' \
+                       '.......m\n' \
+                       '.......m\n' \
+                       'rr..4..m'
+        game = generic_game()
+        state = generic_state(Board(board_string))
+        self.assertFalse(game._disallow_state(state),
+                         'Unexpectedly failed state that has at least one'
+                         ' wildcard and enough of any color to use it:\n{}'
+                         ''.format(state.board))
+
+    def test__disallow_state_disallows_too_few_x_m_s_or_color(self):
+        # combined specification for simplicity
+        board_strings = ('........\n'
+                         '........\n'
+                         '........\n'
+                         '.......m\n'
+                         '........\n'
+                         '.......m\n'
+                         '.......m\n'
+                         'xx.....m',
+
+                         '........\n'
+                         '........\n'
+                         '........\n'
+                         '.......x\n'
+                         '........\n'
+                         '.......x\n'
+                         '.......x\n'
+                         'mm.....x',
+
+                         '........\n'
+                         '........\n'
+                         '........\n'
+                         '.......m\n'
+                         '........\n'
+                         '.......m\n'
+                         '.......m\n'
+                         'ss.....m',
+
+                         '........\n'
+                         '........\n'
+                         '........\n'
+                         '.......m\n'
+                         '........\n'
+                         '.......m\n'
+                         '.......m\n'
+                         'rr.....m')
+        game = generic_game()
+        for board_string in board_strings:
+            state = generic_state(board=Board(board_string))
+            self.assertTrue(game._disallow_state(state),
+                            'This board unexpectedly passed impossible count:'
+                            '\n{}'.format(board_string))
+
+
+def generic_game():
+    """Simple factory to help keep tests focused."""
+    return Game(False)
+
+
+def generic_state(board=None, turn=1, actions_remaining=1):
+    """Simple factory to help keep tests short and focused."""
+    board = board or Board()
+    v = (0, 0)
+    one_actor = Actor('capture', v, v, v, v, v, v, v, v, v)
+    return State(board, one_actor, one_actor, turn, actions_remaining)
+
+
+if __name__ == '__main__':
+    unittest.main()
