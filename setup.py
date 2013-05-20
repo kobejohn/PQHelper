32bfe5e812eee10be1efeb01105bf3fc3b1c72dc
John Nieri <niericentral@gmail.com>
2013-05-20 20:19:02 +0900

    Update setup.py and change to use distribute with backup distribute bootstrap.

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
 
 setup(
     name='PQHelper',
-    version='0.0.0',
-    packages=[''],
-    requires=['numpy', 'treenode'],
-    tests_require=['mock'],
+    version='0.1.0',
+    py_modules=['distribute_setup'],
+    packages=['pqhelper'],
+    requires=['numpy', 'treenode', 'investigators'],
+    tests_require=['mock', 'numpy', 'treenode', 'investigators'],
+    include_package_data=True,
     url='http://github.com/kobejohn/PQHelper',
     license='MIT',
     author='KobeJohn',
     author_email='niericentral@gmail.com',
-    description='Simulate, analyze, and advise on the game called Puzzle Quest '
+    description='Analyze, and advise on the game called Puzzle Quest.',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Programming Language :: Python :: 2.7']
 )
