e92d7882b7a2a628f41f853b5d3a8bf371eb2531
John Nieri <niericentral@gmail.com>
2013-05-29 01:43:08 +0900

    Updated top level solution interface to run multiple simulations and average the results of each action to smooth out the effects of random drops.

--- pqhelper/__init__.py
+++ pqhelper/__init__.py
@@ -9,16 +9,32 @@ Board = base.Board  # top level access for convenience
 Actor = base.Actor  # top level access for convenience
 
 
-def summarize_versus_options(turns=1):
-    board, player, opponnet = _state_investigator.get_versus()
+def summarize_versus_options(turns=2, sims_to_average=2):
+    """Run several simulations and average the results."""
+    board, player, opponent = _state_investigator.get_versus()
     if board is None:
         return tuple()
-    _versus_advisor.reset(board, player, opponnet)
-    for i in range(turns):
-        _versus_advisor.simulate_next_turn()
-    summaries = _versus_advisor.sorted_current_summaries()
-    return summaries
-
+    summaries_by_action = dict()
+    # simulate as many times as specified
+    for i in range(sims_to_average):
+        _versus_advisor.reset(board, player, opponent)
+        for j in range(turns):
+            _versus_advisor.simulate_next_turn()
+        summaries = _versus_advisor.sorted_current_summaries()
+        # group the summaries by the action for easy averaging later
+        for summary in summaries:
+            action = summary['action']
+            summaries_by_action.setdefault(action, list()).append(summary)
+    averaged_summaries = list()
+    for action, summaries in summaries_by_action.items():
+        new_summary = dict()
+        new_summary['action'] = summaries[0]['action']
+        score_sum = sum(summary['overall'] for summary in summaries)
+        score_avg = score_sum / len(summaries)
+        new_summary['overall'] = score_avg
+        averaged_summaries.append(new_summary)
+    averaged_summaries.sort(key=lambda s: s['overall'], reverse=True)
+    return averaged_summaries
 
 def solve_capture():
     board = _state_investigator.get_capture()
