ff517a9b41af5c37541d6869ed508ede39a2a92d
John Nieri <niericentral@gmail.com>
2013-05-28 12:49:51 +0900

    Added convenience methods to the pqhelper __init__.py so that versus and capture can be done in a single step without needing to worry about other details.

---
+++ pqhelper/__init__.py
@@ -0,0 +1,28 @@
+import base, capture, versus
+
+
+_state_investigator = base.StateInvestigator()
+_versus_advisor = versus.Advisor()
+
+
+def summarize_versus_options(turns=1):
+    board, player, opponnet = _state_investigator.get_versus()
+    if board is None:
+        return tuple()
+    _versus_advisor.reset(board, player, opponnet)
+    for i in range(turns):
+        _versus_advisor.simulate_next_turn()
+    summaries = _versus_advisor.sorted_current_summaries()
+    return summaries
+
+
+def solve_capture():
+    board = _state_investigator.get_capture()
+    if board is None:
+        return tuple()
+    steps = capture.capture(board)
+    return steps
+
+
+if __name__ == '__main__':
+    pass
